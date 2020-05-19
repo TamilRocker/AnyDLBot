4d70186a1f0d83f5e3f21ba7e1aee2f6cedd90f2
TamilRocker <63846945+TamilRocker@users.noreply.github.com>
2020-05-19 07:33:07 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,14 +1,14 @@
 class Translation(object):
     START_TEXT = """👋Hi I'm TR Anydl Bot😊
     /help to know how to use me
-  For using me Contact:@Ultalegends01
+  For using me Contact:@Ultralegends01
     👉 I Can Upload Files To Telegram 
   SUPPORT CHANNEL:@TR_Moviezss 
     Just Send Me A File Or Direct Link 😘 """
     
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
-    UPGRADE_TEXT = "Contact @Ultalegends01 to Upgrade "
+    UPGRADE_TEXT = "Contact @Ultralegends01 to Upgrade "
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
